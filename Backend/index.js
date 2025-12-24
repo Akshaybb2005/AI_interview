93141224cd50e18732c6d3f25d39b3f3b402791e
Akshay <akshaykumarbb058@gmail.com>
2025-12-25 00:48:25 +0530

    All interview routes working properly

--- Backend/index.js
+++ Backend/index.js
@@ -3,16 +3,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import session from 'express-session';
 dotenv.config();
-// import { GoogleGenAI } from '@google/genai';
+import { GoogleGenAI } from '@google/genai';
 import { init, init_Chathistory } from './services/pdf.js';
 import authRoutes from './routes/Authroutes.js';
 import interviewRoutes from './routes/InterviewRoutes.js';
-// const ai = new GoogleGenAI({
-//   apiKey: process.env.GOOGLE_API_KEY
-// });
-// app.locals.ai=ai;
 const app = express();
 const port = 3000;
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_API_KEY
+});
+app.locals.ai=ai;
+
 
 app.use(express.json());
 app.use(session({
